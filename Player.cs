841ffa85ecd867779f9ab51d7f4ead9bc0933129
Roshani-Poojari <135858937+Roshani-Poojari@users.noreply.github.com>
2024-07-16 12:33:23 +0530

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerApp.Models
+{
+    internal class Player
+    {
+        private const int DEFAULT_AGE = 18;
+        public int Id { get; }
+        public string Name { get; }
+        public int Age { get; }
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Age = DEFAULT_AGE;
+        }
+        public Player(int id, string name, int age) : this(id, name)
+        {
+            Age = age;
+        }
+        public static Player WhoIsElderPlayer(Player[] players)
+        {
+            Player elderPlayer = null;
+            int playerAge = DEFAULT_AGE;
+            foreach (var player in players)
+            {
+                if (player.Age > playerAge)
+                {
+                    playerAge = player.Age;
+                    elderPlayer = player;
+                }
+            }
+            return elderPlayer;
+        }
+        public override string ToString()
+        {
+            return $"Eldest Player : {Name} \n" +
+                $"Id: {Id} \n" +
+                $"Age: {Age} ";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Principal;
+using PlayerApp.Models;
+
+namespace PlayerApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[]{ 
+                new Player(1,"Swati"),
+                new Player(2,"Roshani",21),
+                new Player(3,"Shweta",22),
+                new Player(4,"Sampada"),
+                new Player(5,"Swamini",20)};
+            Player eldestPlayer = Player.WhoIsElderPlayer(players);
+            Console.WriteLine(eldestPlayer);
+        }
+
+    }
+}
